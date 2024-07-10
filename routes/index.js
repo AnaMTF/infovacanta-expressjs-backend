853f8fe76cd007a35234cbe6839a29e4331abb35
Anamaria <anamaria.titeche@gmail.com>
2024-07-10 21:23:32 +0300

    refactor: Update coordinates handling in routes/index.js
    This commit updates the coordinates handling in the routes/index.js file. It modifies the SQL commands to include latitude and longitude in the getReviewCards queries. This enhancement improves the functionality of the application by providing more accurate location information for review cards.

--- routes/index.js
+++ routes/index.js
@@ -33,7 +33,8 @@ const getDestinationsCommand = "WITH processed_destinations AS ( \
         destination_id, \
         destination_name, \
         destination_category, \
-        coordinates, \
+        coordinates[0] as lat, \
+        coordinates[1] as lon, \
         destination_picture_id, \
         description, \
         replace(lower(unaccent(destination_name)), ' ', '-') as destination_link \
@@ -44,7 +45,8 @@ SELECT \
     pd.destination_id, \
     pd.destination_name, \
     pd.destination_category, \
-    pd.coordinates, \
+    pd.lat, \
+    pd.lon, \
     pd.destination_picture_id, \
     pd.description, \
     pd.destination_link, \
@@ -421,6 +423,8 @@ router.route("/query/destinations/:destinationLink")
 
 router.route("/query/:keyword")
   .get(async function (req, res) {
+    // TO DO:
+    // UNACCENT() function
     const { queryReviewCardsByKeyword } = require("../utils/sql_commands");
     const command_destinations = getDestinationsCommand + " WHERE LOWER(destination_name) LIKE '%' || LOWER($1) || '%' OR LOWER(description) LIKE '%' || LOWER($1) || '%' OR destination_category::TEXT LIKE '%' || LOWER($1) || '%'";
     const command_users = getUsersCommand + " WHERE LOWER(full_name) LIKE '%' || LOWER($1) || '%' OR LOWER(nickname) LIKE '%' || LOWER($1) || '%'";
